0bcfa44cb422cfb551182d3644d5b70df1374893
Mario Yuwono <59589638+marioyuwono@users.noreply.github.com>
2026-09-01 10:11:46 +0700

    Show timestamp on index page

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
 	const [loading, setLoading] = useState(true)
 	const [ref, setRef] = useState<iRef | undefined>(undefined)
 	const [total, setTotal] = useState(0)
+	const [lastUpdate, setLastUpdate] = useState(0)
 	const url = `/api/data/all`
 
 	const load = useCallback(() => {
@@ -26,11 +27,14 @@ export default function Home() {
 			.then(data => {
 				const values = {} as iRef
 				let sum = 0
+				let lastUpdated = 0
 				data.ls.forEach((row: IRow) => {
+					lastUpdated = Math.max(lastUpdated, row.ts)
 					values[row.s] = row.v
 					sum += row.v
 				})
 				setTotal(sum)
+				setLastUpdate(lastUpdated)
 				setRef(values)
 				setLoading(false)
 			})
@@ -47,9 +51,10 @@ export default function Home() {
 			<div className="hero-overlay bg-white dark:bg-gray-900"></div>
 			<div className="hero-content min-h-screen text-neutral-content text-center items-start pt-16">
 				<div className="flex flex-col max-w-sm">
-					<div className='flex gap-3'>
-						<div className='text-4xl text-black dark:text-gray-300'>{loading ? '.' : total}</div>
-						<button className='ms-auto' onClick={() => load()} disabled={loading}>
+					<div className='flex items-center gap-3'>
+						<div className='text-4xl text-black text-left dark:text-gray-300'>{loading ? '.' : total}</div>
+						<FormatDate ts={lastUpdate} className="text-xs text-teal-500 italic ms-auto" />
+						<button className="cursor-pointer" onClick={load} disabled={loading}>
 							<Reload className={`size-6 text-black dark:text-gray-300${loading ? ' animate-spin items-center' : ''}`} />
 						</button>
 					</div>
@@ -75,4 +80,22 @@ export default function Home() {
 			</div>
 		</div >
 	)
+}
+
+function FormatDate({ ts, className }: { ts: number, className?: string }) {
+	if (ts == 0) {
+		return null
+	} else {
+		const dt = new Date(ts)
+		const pad = (n: number) => String(n).padStart(2, '0')
+		return (
+			<p className={className}>
+				{pad(dt.getHours())}
+				:
+				{pad(dt.getMinutes())}
+				:
+				{pad(dt.getSeconds())}
+			</p>
+		)
+	}
 }
--- types/row.ts
+++ types/row.ts
@@ -3,5 +3,5 @@ export interface IRow {
 	id: string
 	s: string
 	v: number
-	ts: Date
+	ts: number
 }
